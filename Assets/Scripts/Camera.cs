9cab8d8632fb60f2107c84bf613399ce87e88205
Sergio Basurco Mancisidor <sergio-mb@sergio-mb.local>
2015-01-24 00:19:49 +0100

    Parallax background with customisable layers and speeds

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	private Texture2D wallLeft;
+	private Texture2D wallRight;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
