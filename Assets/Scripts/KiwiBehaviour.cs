6bf7802e79e83f84a0c8436e70a2eb00e5daddcd
matilla <matilla.martin@gmail.com>
2015-01-24 21:29:57 +0100

    Jumpinggit add -A! :')

--- Assets/Scripts/KiwiBehaviour.cs
+++ Assets/Scripts/KiwiBehaviour.cs
@@ -10,7 +10,7 @@ public class KiwiBehaviour : MonoBehaviour
 	private bool jumping = false;
 	private bool mustJump = false;
 	private Vector2 jumpDestination; 
-
+	[SerializeField] float v; // velocity
 	// Gameloop functions
 
     void Start()
@@ -27,11 +27,63 @@ public class KiwiBehaviour : MonoBehaviour
 	    }
 		else if (!jumping)
 		{
+			Debug.Log("jump dest" + jumpDestination.ToString());
+			Debug.Log("rigidbody" + rigidbody2D.transform.position.ToString());
 			float deltaX = jumpDestination.x - rigidbody2D.transform.position.x;
-			float finalTime = 1.0f;
-			float velocityX = deltaX * finalTime;
+			float deltaY = jumpDestination.y - (rigidbody2D.transform.position.y - collider2D.bounds.size.y/2);
+			float g = -Physics.gravity.y;
+			float x = deltaX; // target x
+			float y = deltaY; // target y
+			Debug.Log ("x: " + x.ToString());
+			Debug.Log ("y: " + y.ToString());
+			float o = float.NaN;
+			float goodO = 0.0f;
+			float goodV = 0.0f;
+			bool bGotNonNan = false;
+			int iterations = 0;
+			bool iterate = true;
+			if(iterate)
+			{
+				do
+				{
+					float s = (v * v * v * v) - g * (g * (x * x) + 2.0f * y * (v * v)); //substitution
+					o = Mathf.Atan((((v * v) + Mathf.Sqrt(s)) / (g * x))); // launch angle
+					Debug.Log (v);
+					iterations++;
+					Debug.Log (o);
+					if(!float.IsNaN(o))
+					{
+						goodO = o;
+						goodV = v;
+						bGotNonNan = true;
 
-			float velocityY = ( 2.0f * (jumpDestination.y - rigidbody2D.transform.position.y + collider2D.bounds.size.y * 0.5f) - (Physics.gravity.y * finalTime) ) / 2.0f;
+					}
+					v -= 0.5f;
+				} while(!float.IsNaN(o) && iterations < 50);
+				if(!bGotNonNan)
+				{
+					Debug.Log ("Cannot jump! need more velocity");
+					return;
+				}
+				else{
+					o = goodO;
+					v = goodV;
+				}
+			}
+			else
+			{
+				float s = (v * v * v * v) - g * (g * (x * x) + 2.0f * y * (v * v)); //substitution
+				o = Mathf.Atan((((v * v) + Mathf.Sqrt(s)) / (g * x))); // launch angle
+			}
+				
+			Debug.Log ("v: " + v.ToString());
+			Debug.Log ("o: " + o.ToString());
+
+			float velocityX = (v*Mathf.Cos(o));
+			float velocityY = (v*Mathf.Sin(o));
+
+//			Debug.Log (Mathf.Sin(o));
+//			Debug.Log (Mathf.Cos(o));
 
 			rigidbody2D.velocity = new Vector2(velocityX, velocityY);
 
