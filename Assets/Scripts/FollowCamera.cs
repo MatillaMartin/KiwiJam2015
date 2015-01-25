58e92e0c049fb5f7403b7caca50b1a4083de89d5
Sergio Basurco Mancisidor <sergio-mb@sergio-mb.local>
2015-01-25 13:49:31 +0100

    pushing

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public bool follow = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(follow)
+			transform.position = Camera.main.transform.position;
+	}
+}
