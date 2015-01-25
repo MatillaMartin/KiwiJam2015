39aa9b5817d3eb498ec4bec6bc3771d6a6fef774
matilla <matilla.martin@gmail.com>
2015-01-25 04:15:21 +0100

    Added kiwi spawner to test speeds etc

---
+++ Assets/Scripts/KiwiSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KiwiSpawner : MonoBehaviour {
+
+	[SerializeField] GameObject m_kiwi_prefab;
+	[SerializeField] float m_minSpeed;
+	[SerializeField] float m_maxSpeed;
+	[SerializeField] float m_spawnTime;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnKiwi ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnKiwi()
+	{
+		while(true)
+		{
+			float speed = Random.Range(m_minSpeed, m_maxSpeed);
+			SpawnKiwi(speed);
+			yield return new WaitForSeconds(m_spawnTime);
+		}
+	}
+	void SpawnKiwi(float speed)
+	{
+		GameObject kiwi = (GameObject)GameObject.Instantiate (m_kiwi_prefab, this.transform.position, Quaternion.identity	);
+		kiwi.GetComponent<KiwiBehaviour> ().velocityX = speed;
+	}
+}
